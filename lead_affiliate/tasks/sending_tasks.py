cabf99b29d5ed557ce47508b444dc19d78d0ca45
nimish <nimish4july1998@gmail.com>
2019-07-13 11:02:38 +0530

    addind meta data in tenant owner referral leads

--- lead_affiliate/tasks/sending_tasks.py
+++ lead_affiliate/tasks/sending_tasks.py
@@ -4,7 +4,7 @@ import requests
 from decouple import config
 
 from referrals.utils import TENANT_LEAD_FIELDS_PRESENT_IN_TENANT_REFERRAL_FIELDS, SUCCESS, \
-    OWNER_LEAD_FIELDS_PRESENT_IN_OWNER_REFERRAL_FIELDS, SOURCE_NAME, DATA
+    OWNER_LEAD_FIELDS_PRESENT_IN_OWNER_REFERRAL_FIELDS, SOURCE_NAME, DATA, METADATA
 from utility.response_utils import STATUS
 from utility.url_constants import TENANT_REFERRAL_CREATE_LEAD_API_URL, OWNER_REFERRAL_CREATE_LEAD_API_URL, \
     TENANT_REFERRAL_BULK_CREATE_LEAD_API_URL, OWNER_REFERRAL_BULK_CREATE_LEAD_API_URL
@@ -17,6 +17,7 @@ def send_tenant_referral_to_lead_tool_to_generate_lead(referral, referral_lead_s
         if field.name in TENANT_LEAD_FIELDS_PRESENT_IN_TENANT_REFERRAL_FIELDS:
             data[field.name] = str(getattr(referral, field.name))
 
+    data[METADATA] = {'referral_id': referral.id}
     request_data[DATA] = data
     req = requests.post(TENANT_REFERRAL_CREATE_LEAD_API_URL, data=json.dumps(request_data),
                         headers={'Content-type': 'application/json'},
@@ -36,6 +37,7 @@ def send_owner_referral_to_lead_tool_to_generate_lead(referral, referral_lead_so
         if field.name in OWNER_LEAD_FIELDS_PRESENT_IN_OWNER_REFERRAL_FIELDS:
             data[field.name] = str(getattr(referral, field.name))
 
+    data[METADATA] = {'referral_id': referral.id}
     request_data[DATA] = data
     req = requests.post(OWNER_REFERRAL_CREATE_LEAD_API_URL, data=json.dumps(request_data),
                         headers={'Content-type': 'application/json'},
@@ -58,6 +60,7 @@ def send_tenant_csv_referral_to_lead_tool_to_generate_leads(tenant_referrals, re
             if field.name in TENANT_LEAD_FIELDS_PRESENT_IN_TENANT_REFERRAL_FIELDS:
                 data[field.name] = str(getattr(referral, field.name))
 
+        data[METADATA] = {'referral_id': referral.id}
         tenant_referral_detail_list.append(data)
 
     request_data[DATA] = tenant_referral_detail_list
@@ -85,6 +88,7 @@ def send_owner_csv_referral_to_lead_tool_to_generate_leads(owner_referrals, refe
             if field.name in OWNER_LEAD_FIELDS_PRESENT_IN_OWNER_REFERRAL_FIELDS:
                 data[field.name] = str(getattr(referral, field.name))
 
+        data[METADATA] = {'referral_id': referral.id}
         owner_referral_detail_list.append(data)
 
     request_data[DATA] = owner_referral_detail_list
