817b6033e9e535c88dc23b5ccdff343461bb9d8f
nimish <nimish4july1998@gmail.com>
2019-07-15 14:04:21 +0530

    changed development settings for halanx affiliate backend for mysql

--- HalanxAffiliateMarketingTool/settings/development.py
+++ HalanxAffiliateMarketingTool/settings/development.py
@@ -12,9 +12,17 @@ ENVIRONMENT_COLOR = '#808080'
 
 # Database settings
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.mysql',
+        'NAME': 'HalanxAffiliate',
+        'USER': 'root',
+        'PASSWORD': 'test',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
